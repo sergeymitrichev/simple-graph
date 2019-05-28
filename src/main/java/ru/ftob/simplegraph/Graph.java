4473aad24035cff6d3d7325b3c66cda0f622a924
sergeymitrichev <sergeymitrichev@yandex.ru>
2019-05-28 13:35:11 +0300

    Add getPath implementation

--- src/main/java/ru/ftob/simplegraph/Graph.java
+++ src/main/java/ru/ftob/simplegraph/Graph.java
@@ -8,6 +8,8 @@ public class Graph<T> {
 
   private Map<Vertex<T>, List<Vertex<T>>> vertices = new HashMap<>();
 
+  private Set<Vertex<T>> visited = new HashSet<>();
+
   public Graph() {
     this(false);
   }
@@ -17,9 +19,21 @@ public class Graph<T> {
   }
 
   public void addVertex(T type) {
-    vertices.putIfAbsent(new Vertex<>(type), new ArrayList<>());
+    addVertex(new Vertex<>(type));
   }
 
+  public void addVertex(Vertex<T> vertex) {
+    vertices.putIfAbsent(vertex, new ArrayList<>());
+  }
+
+  /**
+   * Adds edge to graph by vertices types. Direction of arc (directed graph): from --&gt; to
+   *
+   * @param from vertex type value of edge
+   * @param to vertex type value of edge
+   * @throws IllegalArgumentException if edge vertices are not
+   * part of graph
+   */
   public void addEdge(T from, T to) {
     Vertex<T> v1 = new Vertex<>(from);
     Vertex<T> v2 = new Vertex<>(to);
@@ -36,6 +50,40 @@ public class Graph<T> {
 
   }
 
+  /**
+   * Adds edge to graph by vertices. Direction of arc (directed graph): from --&gt; to
+   *
+   * @param from vertex type value of edge
+   * @param to vertex type value of edge
+   * @throws IllegalArgumentException if edge vertices are not
+   * part of graph
+   */
+  public void addEdge(Vertex<T> from, Vertex<T> to) {
+    addEdge(from.getType(), to.getType());
+  }
+
+  /**
+   * Adds edge to graph.
+   *
+   * @param edge edge to add
+   * @throws IllegalArgumentException if edge vertices are not
+   * part of graph
+   */
+  public void addEdge(Edge<T> edge) {
+    addEdge(edge.getFrom(), edge.getTo());
+  }
+
+  /**
+   * Returns list of edges of the found path. If the path from the start
+   * point to the end point is not found, an empty sheet is returned.
+   * Path is not optimal.
+   *
+   * @param from starting point of the path
+   * @param to the endpoint of the path
+   * @return list of edges of the found path
+   * @throws IllegalArgumentException if the start or end points are not
+   * part of the graph
+   */
   public List<Edge<T>> getPath(T from, T to) {
     Vertex<T> v1 = new Vertex<>(from);
     Vertex<T> v2 = new Vertex<>(to);
@@ -57,6 +105,17 @@ public class Graph<T> {
       return Arrays.asList(from, to);
     }
 
+    visited.add(from);
+
+    for (Vertex<T> node : vertices.get(from)) {
+      if (!visited.contains(node)) {
+        ArrayList<Vertex<T>> subPath = new ArrayList<>(getPathByBreadthFirstSearch(node, to));
+        if (subPath.contains(to)) {
+          subPath.add(0, from);
+          return subPath;
+        }
+      }
+    }
     return Collections.emptyList();
   }
 
